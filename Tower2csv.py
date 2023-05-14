910bf8cfa75debc18f26a0f3fd0e72a0b5662083
marcosp-araujo <88653954+marcosp-araujo@users.noreply.github.com>
2023-05-14 16:25:56 +0200

    Tower2csv.py

--- Tower2csv.py
+++ Tower2csv.py
@@ -41,22 +41,24 @@ class Tower2csv:
         df_year = pd.DataFrame()
         for sensor_dir in os.listdir(self.netcdf_dir):
             print('Reading', sensor_dir,'files')
-            current_dir = (os.path.join(self.netcdf_dir, sensor_dir)) #current dir
+            current_dir = (os.path.join(self.netcdf_dir, sensor_dir)) #current directory
             for current_file in os.listdir(current_dir):
                 if current_file.find(self.year) >= 0:
                     files_dir.append(os.path.join(current_dir, current_file))
                     files_names.append(current_file)
                     sensors_names.append(current_file.split('_')[0]) 
-                    #print(files_dir[-1])
-                  #Converted method
+                  #print(files_dir[-1])
+                  #Calling coverted method
                     df = self.nc2df(files_dir[-1])
                     df_concat = pd.concat([df_concat,df])
             df_year = pd.concat([df_year,df_concat],axis = 1)
             df_concat = pd.DataFrame()
+            
         self.sensors_names = np.unique(sensors_names)     
         self.files_dir = files_dir
         self.files_names = files_names           
         self.df_year = df_year
+        self.df_concat = df_concat
         
 ###############################################################################  
 ## This method reads and converts netcdf into dataframe       
@@ -65,13 +67,14 @@ class Tower2csv:
         df = nc.to_dataframe()     #Converting netcdf into dataframe
        #Dropping "latitude", "longitude" and "height" indexes
         df = df.droplevel(['latitude','longitude','height']) 
-        sensor = df.columns[0]; #It is the main quality-assured column
+        sensor = df.columns[0]; #It is the main quality-assured column 
        #Keep only the quality-assured main current_sensor 
        #(the others are 'quality_flag' or 'raw_data')
         df = df[[sensor]]
        #Asignin NaN to values equals to -99999
         df[sensor] [( df[sensor] < -9999)] = np.nan
-         
+        return df
+    
 ###############################################################################
 ## This method saves the dataframe in .csv format
     def save_csv(self):
