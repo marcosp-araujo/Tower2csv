513290861273a0e5c940ce09ac02f6433c3617a8
marcosp-araujo <88653954+marcosp-araujo@users.noreply.github.com>
2023-05-21 20:22:28 +0200

    main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from Tower2csv import Tower2csv
 
 netcdf_dir = os.getcwd() + '/owez/10minutely/'; #directory of the netcdf files
 year = "2005";           #year that to be converted
-save_dir = os.getcwd() + '/owez/csv/'; #directory to save the .csv files
+save_dir = os.getcwd() + '/owez/'; #directory to save the .csv files
 
 C = Tower2csv(netcdf_dir,year,save_dir)
