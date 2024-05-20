4351e8259a2a4215998172988bce24b1900d0184
Marcos Araujo <marcos.araujo@eolossolutions.com>
2024-05-20 18:38:38 +0200

    update in the main file

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ Main script example on how to use the Tower2csv.py
 '''
 
 #%% 
-import os
 from Tower2csv import Tower2csv
 
-netcdf_dir = './owez/10minutely/' # directory of the netCDF files
-save_dir = os.getcwd()  # directory to save the .csv file
+netcdf_dir = "./owez/10minutely/" # directory of the netCDF files
+save_dir = "./"  # directory to save the .csv file
 save_file_name = "owez" # year to be converted
 
 T = Tower2csv(netcdf_dir, save_dir, save_file_name)
-
-# %%
