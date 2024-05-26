02af146eeef94856a666c8cdf85372076c479800
Marcos Araujo <marcos.araujo@eolossolutions.com>
2024-05-26 15:16:32 +0200

    update at readme and main

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ https://github.com/marcosp-araujo/Tower2csv/
 from Tower2csv import Tower2csv
 
 netcdf_dir = "./owez/10minutely/" # directory of the netCDF files
-save_dir = "./"  # directory to save the .csv file
-save_file_name = "owez" # year to be converted
+save_dir = "./"                   # directory to save the .csv file
+save_file_name = "owez"           # year to be converted
 
 T = Tower2csv(netcdf_dir, save_dir, save_file_name)
