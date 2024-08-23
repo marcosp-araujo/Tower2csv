4d2d58da3368061b36c36fc30fabaae30c8bc0b1
Marcos Araujo <marcos.araujo@eolossolutions.com>
2024-08-23 20:14:22 +0200

    Improving the project description

--- app_modules/description.py
+++ app_modules/description.py
@@ -6,13 +6,14 @@ def description():
     ttower_dowload_page = 'https://talltowers.bsc.es/access-the-data'
 
     st.title('Tower2csv API')
-    
+
     st.write('By Marcos da Silva [(Linkedin)](%s)' %'https://www.linkedin.com/in/marcosp-araujo/')
 
-    st.write('The Tower2csv is a Python-based API aimed at accessing global wind energy measurement data with ease. Its user-friendly interface (built with Streamlit) provides the downloading of data from the [Tall Tower Dataset](%s) with only three clicks. This API automatically performs the following tasks:' % ttower_main_page)
+    st.write('The Tower2csv is a Python-based API aimed at accessing global wind energy measurement data with ease. Its user-friendly interface (built with Streamlit) provides the downloading of data from the [Tall Tower Dataset](%s) in CSV format with only three clicks. This API automatically performs the following tasks:' % ttower_dowload_page)
 
-    st.markdown('- Requests a dataset to the Tall Tower')
-    st.markdown('- Unzip the netCDF files')
-    st.markdown('- Reads and concatenates all netCDF files')
-    st.markdown('- Delivers the output dataset in a single .CSV for downloading')
-    st.write('Detailed information on the available meteorological towers can be found at the [Tall Tower download page](%s)' % ttower_dowload_page)
+    st.write('''
+             - Request a dataset to the Tall Tower
+             - Unzip the netCDF files 
+             - Read and concatenate all netCDF files
+             - Deliver the dataset in a single CSV file for downloading
+             ''')
