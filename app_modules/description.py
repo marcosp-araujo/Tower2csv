12d7c48d4066c406ae8e96e11038f1e86d602720
Marcos Araujo <marcos.araujo@eolossolutions.com>
2024-08-23 19:28:06 +0200

    Improving the project description

--- app_modules/description.py
+++ app_modules/description.py
@@ -5,7 +5,7 @@ def description():
     ttower_main_page = 'https://talltowers.bsc.es/'
     ttower_dowload_page = 'https://talltowers.bsc.es/access-the-data'
     st.write('The Tower2csv is a Python-based API aimed at accessing global wind energy measurement data with ease. Its user-friendly interface (built with Streamlit) provides the downloading of data from the [Tall Tower Dataset](%s) with only three clicks. This API automatically performs the following tasks:' % ttower_main_page)
-    st.markdown('- Automatically requests a dataset to the Tall Tower')
+    st.markdown('- Requests a dataset to the Tall Tower')
     st.markdown('- Unzip the netCDF files')
     st.markdown('- Reads and concatenates all netCDF files')
     st.markdown('- Delivers the output dataset in a single .CSV for downloading')
