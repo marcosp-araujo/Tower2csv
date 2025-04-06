ce38e46fecf761da11a1ed96156c1c2151908f74
Marcos Araujo <marcos.araujo@eolossolutions.com>
2025-04-06 12:58:32 +0200

    Adding updated date

--- app_modules/description.py
+++ app_modules/description.py
@@ -11,7 +11,7 @@ def description():
     
     st.title('Tower2csv API')
 
-    st.write('By Marcos da Silva [(Linkedin)](%s), on April 6, 2025.' %'https://www.linkedin.com/in/marcosp-araujo/')
+    st.write('By Marcos da Silva [(Linkedin)](%s). Last updated on April 6, 2025.' %'https://www.linkedin.com/in/marcosp-araujo/')
 
     st.markdown("""
     <p style='text-align: justify;'>
